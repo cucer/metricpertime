e62815f8194696b66a7b53bcae15ffa5cc157a67
Cagatay Ucer <cagatay.ucer@gmail.com>
2021-11-29 14:00:02 +0300

    analytics

--- src/App.js
+++ src/App.js
@@ -1,7 +1,14 @@
+import React, { useEffect } from 'react'
+import ReactGa from 'react-ga'
 import './css/App.css'
 import Main from './components/Main'
 
 function App() {
+  useEffect(() => {
+    ReactGa.initialize('UA-62711254-5')
+    ReactGa.pageview('/metrics')
+  }, [])
+
   return (
     <div className='app'>
       <Main />
